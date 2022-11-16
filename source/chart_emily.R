97af0db14420b92e04c7e258d549cba40bd8db6f
Emily <echang35@uw.edu>
2022-11-15 23:17:07 -0800

    chart 2

---
+++ source/chart_emily.R
@@ -0,0 +1,16 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+snowcrab_df <- read.csv("/Users/emilyayano/documents/info201/project-emilynakaone/data/mfsnowcrab.csv")
+
+crabhaul_df <- snowcrab_df %>% 
+  select(year, haul) %>% 
+  group_by(year) %>% 
+  summarise(across(c(haul), sum)) 
+  
+scatterplot_chart <- ggplot(crabhaul_df, aes(x = year, y=haul)) +
+  geom_point(size=2, shape=1) +
+  labs(title = "Number of Hauls per Year") +
+  scale_x_continuous(n.breaks = 9)
+
