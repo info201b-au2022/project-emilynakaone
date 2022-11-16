68b64d024f75d24502c20405263a19e96ff4249b
dtmsyi <dtmsyi@gmail.com>
2022-11-15 22:25:25 -0800

    chart 1 and 3 in

--- source/chart_riley.R
+++ source/chart_riley.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-snowcrab_df <- read.csv("mfsnowcrab.csv")
+snowcrab_df <- read.csv("../data/mfsnowcrab.csv")
 
 temps_17_18 <- snowcrab_df  %>% 
   filter(year >= 2012)
@@ -15,7 +15,7 @@ boxplot_chart <- ggplot(temps_17_18) +
                ) +
   scale_y_continuous(breaks = seq(-2,8, by = 1)) +
   xlab("Year (2012-2018)") +
-  ylab("Temperature (°C)") +
+  ylab("Temperature (C)") +
   ggtitle("Ocean Floor Tempurature Distribution Trends") +
   theme(plot.title = element_text(hjust = 0.5))
 
